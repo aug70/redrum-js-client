64ce12e538cb2896c9f85de2f3f2a489a061dc7a
Yalim Aytul <yalim.aytul@gmail.com>
2014-10-21 00:03:55 -0500

    Addig stats and game page.

--- public/js/angular/controllers.js
+++ public/js/angular/controllers.js
@@ -3,7 +3,52 @@
 function DashBoardController($scope, $http) {
 	
 	$scope.init = function() {
-		$scope.message = 'Achtung';
+		$scope.budget = { 
+			box_header : '$143,000',
+			box_text : 'Your budget',
+			box_link : '/market'
+		};
+		$scope.stats = { 
+			box_header : '%56 success',
+			box_text : 'See your stats',
+			box_link : '/stats'
+		};
+		$scope.profile = { 
+			box_header : 'Profile',
+			box_text : 'Edit your profile',
+			box_link : '/profile'
+		};
+		$scope.product = { 
+			box_header : '56',
+			box_text : 'Unique products',
+			box_link : '/market#products'
+		};
+		$scope.redeem = { 
+			box_header : 'Redeem',
+			box_text : 'Use coupons and add to your credit',
+			box_link : '/market#redeem'
+		};
+		$scope.buy = { 
+			box_header : 'Buy',
+			box_text : 'Buy game credit',
+			box_link : '/market#credit'
+		};
+		$scope.shop = { 
+			box_header : 'Shop',
+			box_text : 'Add more products to your inventory',
+			box_link : '/market#cart'
+		};
+		$scope.game = { 
+			box_header : 'Solve',
+			box_text : 'Play a game and solve a murder',
+			box_link : '/game'
+		};
+
+
+
+
+
+		
 	}
 }
 
