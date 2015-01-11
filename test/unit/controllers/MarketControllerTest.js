90f33418485a558c41bacd66b5821cc521d3cd6f
Yalim Aytul <yalim.aytul@gmail.com>
2015-01-10 20:27:02 -0600

    Adding target top for all links.

---
+++ test/unit/controllers/MarketControllerTest.js
@@ -0,0 +1,15 @@
+require('../../bootstrap.test.js');
+var request = require('request');
+var assert = require('assert');
+
+describe('Market controller tests', function() {
+
+	it('Default page', function(done) {
+		request.get("http://localhost:1337/market", function(err, response, body) {
+			assert(!err);
+			assert(body);
+			return done();
+		})
+	});
+
+});
