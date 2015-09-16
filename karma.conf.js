103c045243c62d0431b8adb8b1735fde3dbc88e8
Yalim Aytul <yalim.aytul@gmail.com>
2015-09-15 22:44:32 -0500

    Add chai, remove requirejs.

--- karma.conf.js
+++ karma.conf.js
@@ -10,13 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'requirejs'],
+    frameworks: ['mocha', 'chai'],
 
 
 
     // list of files / patterns to load in the browser
     files: [
-      'test/test-requirejs.js',
       'assets/js/angular/*.js',
       'assets/ng-table/**/*',
       'test/angular/**/*.js'
