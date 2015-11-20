2ab97151fdbc7cb2f3386f97367bcfc949907217
Yalim Aytul <yalim.aytul@gmail.com>
2015-11-19 19:54:18 -0600

    More clean up.

--- assets/js/angular/filters.js
+++ assets/js/angular/filters.js
@@ -5,8 +5,8 @@ angular.module('redrumAppFilters', [])
 
 	.filter('splitRow', function() {
 		return function (input, count){
-			console.log("input:" + input);
-			console.log("count:" + count);
+			//console.log("input:" + input);
+			//console.log("count:" + count);
 			var out = [];
 				if(typeof input === "object"){
 					for (var i=0, j=input.length; i < j; i+=count) {
